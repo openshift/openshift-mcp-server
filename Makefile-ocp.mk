efc2861e508ea12085225b35dd0eecec88c45422
Arda Güçlü <aguclu@redhat.com>
2025-08-18 13:57:51 +0300

    fix

--- Makefile-ocp.mk
+++ Makefile-ocp.mk
@@ -2,4 +2,4 @@ include Makefile
 
 .PHONY: build-ocp
 build-ocp: clean format
-	$(GO_BUILD_ENV) go build $(COMMON_BUILD_ARGS) $(GOFLAGS) -o $(BINARY_NAME) ./cmd/kubernetes-mcp-server
+	CGO_ENABLED=1 $(GO_BUILD_ENV) go build $(COMMON_BUILD_ARGS) -tags=strictfipsruntime -mod=vendor -a -o openshift-mcp-server ./cmd/kubernetes-mcp-server
