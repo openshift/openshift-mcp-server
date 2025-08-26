0ec2599bd8a4cd3543d9755d49c2d414e45814c0
Marc Nuri <marc@marcnuri.com>
2025-08-26 14:52:27 +0200

    fix:test: prevent usage of real cluster in tests (#282)
    Signed-off-by: Marc Nuri <marc@marcnuri.com>

--- pkg/mcp/common_test.go
+++ pkg/mcp/common_test.go
@@ -52,6 +52,9 @@ var envTestUser = envtest.User{Name: "test-user", Groups: []string{"test:users"}
 
 func TestMain(m *testing.M) {
 	// Set up
+	_ = os.Setenv("KUBECONFIG", "/dev/null")     // Avoid interference from existing kubeconfig
+	_ = os.Setenv("KUBERNETES_SERVICE_HOST", "") // Avoid interference from in-cluster config
+	_ = os.Setenv("KUBERNETES_SERVICE_PORT", "") // Avoid interference from in-cluster config
 	envTestDir, err := store.DefaultStoreDir()
 	if err != nil {
 		panic(err)
