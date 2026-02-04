a7b6342fccc062934c4a08c9eb84310bdc413e42
Saswata Mukherjee <saswataminsta@yahoo.com>
2026-02-04 10:14:29 +0000

    Add AM tools to toolset via rhobs/obs-mcp
    Signed-off-by: Saswata Mukherjee <saswataminsta@yahoo.com>

--- pkg/mcp/modules.go
+++ pkg/mcp/modules.go
@@ -7,6 +7,5 @@ import (
 	_ "github.com/containers/kubernetes-mcp-server/pkg/toolsets/kcp"
 	_ "github.com/containers/kubernetes-mcp-server/pkg/toolsets/kiali"
 	_ "github.com/containers/kubernetes-mcp-server/pkg/toolsets/kubevirt"
-	_ "github.com/containers/kubernetes-mcp-server/pkg/toolsets/observability"
 	_ "github.com/rhobs/obs-mcp/pkg/toolset"
 )
