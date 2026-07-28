0e389ba5e038223f98dc040b51ad4540ef61587d
Eric Fried <2uasimojo@users.noreply.github.com>
2026-07-28 12:22:33 -0500

    Allow env var to specify config file path (#1335)
    With this change, if `--config` is not given on the CLI, we will look
    for an environment variable specifying a path to the main config file.
    
    The variable is spelled `$K8S_MCP_CONFIG_PATH` here, but this change
    includes an override mechanism so downstreams can choose their own
    spelling.
    
    Signed-off-by: Eric Fried <efried@redhat.com>

---
+++ pkg/kubernetes-mcp-server/cmd/root_var_overrides.go
@@ -0,0 +1,9 @@
+package cmd
+
+// IMPORTANT: this file is used to override default variable values in downstream builds.
+
+// varOverrides is invoked early in the command bootstrapping process, allowing
+// downstreams to rename things like env var keys.
+func varOverrides() {
+	// Intentionally left blank upstream.
+}
