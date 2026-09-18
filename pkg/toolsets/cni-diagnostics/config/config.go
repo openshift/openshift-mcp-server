c4f52557ea03677fc21595baa7e09cbfb374b447
Calum Murray <cmurray@redhat.com>
2026-09-18 15:21:09 -0400

    chore: fix lint errors
    Signed-off-by: Calum Murray <cmurray@redhat.com>

--- pkg/toolsets/cni-diagnostics/config/config.go
+++ pkg/toolsets/cni-diagnostics/config/config.go
@@ -26,7 +26,7 @@ type Config struct {
 	PwruImage string `toml:"pwru_image,omitempty"`
 }
 
-var _ api.ExtendedConfig = (*Config)(nil)
+var _ config.ExtendedConfig = (*Config)(nil)
 
 // Validate validates the CNI Diagnostics toolset configuration
 func (c *Config) Validate() error {
@@ -50,7 +50,7 @@ func GetConfig(params api.ToolHandlerParams) *Config {
 }
 
 // cnidiagnosticsToolsetParser parses the CNI Diagnostics toolset configuration from TOML
-func cnidiagnosticsToolsetParser(_ context.Context, primitive toml.Primitive, md toml.MetaData) (api.ExtendedConfig, error) {
+func cnidiagnosticsToolsetParser(_ context.Context, primitive toml.Primitive, md toml.MetaData) (config.ExtendedConfig, error) {
 	var cfg Config
 	if err := md.PrimitiveDecode(primitive, &cfg); err != nil {
 		return nil, err
